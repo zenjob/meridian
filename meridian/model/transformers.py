Fix bug in controls transformer where the mean and standard deviation were taken before population scaling, when it should have been after. This bug impacted any controls where the corresponding `population_scaling_id` value was true.
PiperOrigin-RevId: 665404373

meridian/model/transformers.py
@@ -104,11 +104,15 @@ class ControlsTransformer:
           population[:, None],
           tf.ones_like(population)[:, None],
       )
+      population_scaled_controls = (
+          controls / self._population_scaling_factors[:, None, :]
+      )
+      self._means = tf.reduce_mean(population_scaled_controls, axis=(0, 1))
+      self._stdevs = tf.math.reduce_std(population_scaled_controls, axis=(0, 1))
     else:
       self._population_scaling_factors = None
-
-    self._means = tf.reduce_mean(controls, axis=(0, 1))
-    self._stdevs = tf.math.reduce_std(controls, axis=(0, 1))
+      self._means = tf.reduce_mean(controls, axis=(0, 1))
+      self._stdevs = tf.math.reduce_std(controls, axis=(0, 1))
 
   @tf.function(jit_compile=True)
   def forward(self, controls: tf.Tensor) -> tf.Tensor:

meridian/model/transformers_test.py
@@ -168,15 +168,17 @@ class ControlsTransformerTest(absltest.TestCase):
     )
 
   def test_output_population_scaled(self):
-    pop_mean = tf.reduce_mean(self._population)
-    pop_std = tf.math.reduce_std(self._population)
-    expected_output = tf.Variable(self._controls4)
-    expected_output[:, :, 1].assign(1)
-    expected_output[:, :, 3].assign(1)
-    expected_output[:, :, 4].assign(1)
-    tf.debugging.assert_near(
-        self._controls_transformed * pop_std + pop_mean, expected_output
-    )
+    for c in [1, 3, 4]:
+      population_scaled_control = (
+          self._controls4[..., c] / self._population[:, None]
+      )
+      means = tf.reduce_mean(population_scaled_control, axis=(0, 1))
+      stdevs = tf.math.reduce_std(population_scaled_control, axis=(0, 1))
+      tf.debugging.assert_near(
+          self._population[:, None]
+          * (self._controls_transformed[:, :, c] * stdevs + means),
+          self._controls4[:, :, c],
+      )
 
 
 class KpiTransformerTest(absltest.TestCase):